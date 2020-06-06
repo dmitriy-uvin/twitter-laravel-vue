b9e8d9a2ea795bf333032fa363a9c9b6a29c74dc
dmitriy-sanders <dimauuvin@gmail.com>
2020-06-06 15:37:55 +0300

    Add LIKE and DISLIKE mutationsTypes

--- frontend/src/store/modules/comment/mutationTypes.js
+++ frontend/src/store/modules/comment/mutationTypes.js
@@ -3,3 +3,5 @@ export const ADD_COMMENT = 'addComment';
 export const DELETE_COMMENT = 'deleteComment';
 export const UPDATE_COMMENT = 'updateComment';
 export const SET_COMMENT_IMAGE = 'setCommentImage';
+export const LIKE_COMMENT = 'likeComment';
+export const DISLIKE_COMMENT = 'dislikeComment';
