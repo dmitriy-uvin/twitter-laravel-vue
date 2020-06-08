ab4c47928d94b7eb702eba97c1209ab8cd4e0b2c
dmitriy-sanders <dimauuvin@gmail.com>
2020-06-08 22:07:54 +0300

    Small fixes

--- backend/resources/views/emails/likedTweet.blade.php
+++ backend/resources/views/emails/likedTweet.blade.php
@@ -3,7 +3,7 @@
 # Your tweet was liked!
 
 Hello **{{ $receiver->first_name }} {{ $receiver->last_name }}**, we are glad to tell you that your tweet was liked by
-<a href="{{ env('APP_URL') }}/users/{{ $liker->id }}">{{ $liker->first_name }} {{ $liker->last_name }}</a>
+<a href="{{ env('FRONT_URL') }}/users/{{ $liker->id }}">{{ $liker->first_name }} {{ $liker->last_name }}</a>
 
 Your tweet:
 @component('mail::panel')
@@ -14,7 +14,7 @@ Your tweet:
 <img src="{{ $tweet->image_url }}" alt="Tweet image">
 @endif
 
-@component("mail::button", ['url' => env("APP_URL")."/tweets/".$tweet->id, 'color' => 'success'])
+@component("mail::button", ['url' => env("FRONT_URL")."/tweets/".$tweet->id, 'color' => 'success'])
     Go to tweet
 @endcomponent
 
