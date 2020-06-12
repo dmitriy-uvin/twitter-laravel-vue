bf114325279c141deb22f301fed34612aeaddb62
dmitriy-sanders <dimauuvin@gmail.com>
2020-06-12 21:17:58 +0300

    Implement markdown welcome.blade.php

--- backend/resources/views/emails/welcome.blade.php
+++ backend/resources/views/emails/welcome.blade.php
@@ -1,10 +1,11 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Welcome!</title>
-  </head>
-  <body>
-    Welcome to Thread application!
-  </body>
-</html>
+@component("mail::message")
+
+# Welcome to the {{ env("APP_NAME") }}!
+
+Hello **{{ $user->first_name }} {{ $user->last_name }}**, thank you for registering on {{ env("APP_NAME") }}!
+
+@component("mail::button", ['url' => env("FRONT_URL")])
+    Visit {{ env("APP_NAME") }}!
+@endcomponent
+
+@endcomponent
