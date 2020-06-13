4d88bb4fc2a60c08f9e1ffe0136082f463e6fb6b
dmitriy-sanders <dimauuvin@gmail.com>
2020-06-13 19:55:17 +0300

    Fix speed loading with likedUsers (closes #4)

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -3,6 +3,7 @@ import {
     SET_LOADING,
     SET_LIKED_USERS
 } from './mutationTypes';
+import { userMapper } from '../services/Normalizer';
 
 export default {
     async getUsersByIds({ commit }, ids) {
@@ -19,6 +20,23 @@ export default {
             commit(SET_LOADING, false, { root: true });
 
             return true;
+        } catch (error) {
+            commit(SET_LOADING, false, { root: true });
+
+            return Promise.reject(error);
+        }
+    },
+
+    async getUsersByIdsCollection({ commit }, ids) {
+        commit(SET_LOADING, true, { root: true });
+
+        try {
+            const users = await api.post('/users', { usersIds: ids });
+
+            commit(SET_LIKED_USERS, users);
+            commit(SET_LOADING, false, { root: true });
+
+            return Promise.resolve(users.map(userMapper));
         } catch (error) {
             commit(SET_LOADING, false, { root: true });
 
